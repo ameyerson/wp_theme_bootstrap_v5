d2ad3cf1114e33d7965ceb2746b66055b5557c0b
Alexandra Meyerson <alex.meyerson@gmail.com>
2025-05-08 16:28:45 -0400

    updates plugins

--- plugins/wordpress-seo/src/helpers/url-helper.php
+++ plugins/wordpress-seo/src/helpers/url-helper.php
@@ -30,7 +30,7 @@ class Url_Helper {
 			return $home_url;
 		}
 
-		if ( \is_null( $home_path ) ) { // Home at site root, always slash.
+		if ( $home_path === null ) { // Home at site root, always slash.
 			return \trailingslashit( $home_url );
 		}
 
@@ -106,8 +106,7 @@ class Url_Helper {
 	 */
 	public function get_url_path( $url ) {
 		if ( \is_string( $url ) === false
-			&& \is_object( $url ) === false
-			|| ( \is_object( $url ) === true && \method_exists( $url, '__toString' ) === false )
+			&& ( \is_object( $url ) === false || \method_exists( $url, '__toString' ) === false )
 		) {
 			return '';
 		}
@@ -124,8 +123,7 @@ class Url_Helper {
 	 */
 	public function get_url_host( $url ) {
 		if ( \is_string( $url ) === false
-			&& \is_object( $url ) === false
-			|| ( \is_object( $url ) === true && \method_exists( $url, '__toString' ) === false )
+			&& ( \is_object( $url ) === false || \method_exists( $url, '__toString' ) === false )
 		) {
 			return '';
 		}
@@ -187,7 +185,7 @@ class Url_Helper {
 
 		$base_url = \trailingslashit( $url_parts['scheme'] . '://' . $url_parts['host'] );
 
-		if ( ! \is_null( $path ) ) {
+		if ( \is_string( $path ) ) {
 			$base_url .= \ltrim( $path, '/' );
 		}
 
@@ -215,7 +213,7 @@ class Url_Helper {
 			return ( $is_image ) ? SEO_Links::TYPE_EXTERNAL_IMAGE : SEO_Links::TYPE_EXTERNAL;
 		}
 
-		if ( \is_null( $home_url ) ) {
+		if ( $home_url === null ) {
 			$home_url = \wp_parse_url( \home_url() );
 		}
 
@@ -278,5 +276,20 @@ class Url_Helper {
 		}
 
 		return $current_url;
+	}
+
+	/**
+	 * Parses a URL and returns its components, this wrapper function was created to support unit tests.
+	 *
+	 * @param string $parsed_url The URL to parse.
+	 * @return array The parsed components of the URL.
+	 */
+	public function parse_str_params( $parsed_url ) {
+		$array = [];
+
+		// @todo parse_str changes spaces in param names into `_`, we should find a better way to support them.
+		\wp_parse_str( $parsed_url, $array );
+
+		return $array;
 	}
 }
--- plugins/wordpress-seo/vendor/autoload.php
+++ plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f169a3608cd329ef13ae362fe2c82a7::getLoader();
+return ComposerAutoloaderInit18ea696e1cd589184e616540b92e0296::getLoader();
